61dd3822d0ec9009e0ac9a78dfcd53ab604ef2ca
Vestria <118200330+Vestria4@users.noreply.github.com>
2023-12-13 00:10:51 +0000

    Update content.js

--- content.js
+++ content.js
@@ -1,37 +1,25 @@
 // content.js
-// Injected script for https://grabvr.quest/levels*
 (function() {
-  // Check if the URL contains the required parameters
-  const urlParams = new URLSearchParams(window.location.search);
-  const userId = urlParams.get('user_id');
+  const extractUserInfo = () => {
+    // Extract basic user information from the webpage
+    const userIdElement = document.getElementById('user-id');
+    const mainColorElement = document.getElementById('main-color');
+    const secondaryColorElement = document.getElementById('secondary-color');
 
-  if (userId) {
-    // Fetch user data or perform necessary actions
-    // You can make API requests or manipulate the page DOM as needed
-    const userData = {
-      user_id: userId,
-      // Add other relevant data
-    };
+    if (userIdElement && mainColorElement && secondaryColorElement) {
+      const userData = {
+        user_id: userIdElement.textContent,
+        active_customizations: {
+         player_color_primary: { color: mainColorElement.textContent.split(', ') },
+          player_color_secondary: { color: secondaryColorElement.textContent.split(', ') }
+        }
+        // Add other relevant data extraction
+      };
 
-    // Display information on the page
-    const displayInfo = () => {
-      // Modify the page DOM to display user information, colors, roles, etc.
-      // Example: Display user ID and colors in a div
-      const infoDiv = document.createElement('div');
-      infoDiv.innerHTML = `
-        <p>User ID: ${userData.user_id}</p>
-        <p>Main Color: ${userData.active_customizations.player_color_primary.color.join(', ')}</p>
-        <p>Secondary Color: ${userData.active_customizations.player_color_secondary.color.join(', ')}</p>
-        <p>Is Admin: ${userData.is_admin ? 'Yes' : 'No'}</p>
-        <p>Is Moderator: ${userData.is_moderator ? 'Yes' : 'No'}</p>
-        <p>Is Creator: ${userData.is_creator ? 'Yes' : 'No'}</p>
-      `;
+      // Send the user data to the background script
+      chrome.runtime.sendMessage({ action: 'extractUserInfo', data: userData });
+    }
+  };
 
-      // Append the information to the body or a specific container on the page
-      document.body.appendChild(infoDiv);
-    };
-
-    // Call the displayInfo function
-    displayInfo();
-  }
+  extractUserInfo();
 })();
